957f866c6b13682ca1dca3e53af0554d91711c3a
nidhi-1201 <pnidhi03@gmail.com>
2021-10-18 01:59:57 +0530

    jQuery added

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,4 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 465,
-  domain: 'gmail.com',
-  user_name: 'meets9921@gmail.com', #Rails.application.credentials[Rails.env.to_sym][:mailer][:username],
-  password: 'MKS4344!',#Rails.application.credentials[Rails.env.to_sym][:mailer][:password],
-  authentication: 'plain'
-}
+
